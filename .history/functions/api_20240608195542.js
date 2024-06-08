585b39b25f248339eeaeaaf49cd67072a5d68a6b
Roniel <roniellora8@gmail.com>
2024-06-08 19:56:07 +0800

    .

--- .history/functions/api_20240608195542.js
+++ .history/functions/api_20240608195542.js
@@ -8,6 +8,14 @@ const app = express();
 const dbCloudUrl = "mongodb+srv://admin:12345@testdb.22mv9ja.mongodb.net/authorDB";
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
